337e5743e5a94728bf2aa2fa65854f026d61cfc4
Roy <roy200230034004@gmail.com>
2019-05-15 18:20:30 +0800

    first commit

---
+++ IntelligenceCloud/Infrastructure/UserAuthorize.cs
@@ -0,0 +1,72 @@
+﻿using IntelligenceCloud.Helpers;
+using IntelligenceCloud.Models;
+using IntelligenceCloud.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IntelligenceCloud.Infrastructure
+{
+    public class UserAuthorizeAttribute : AuthorizeAttribute
+    {
+        public string AuthorizationFailView { get; set; }
+        private bool accessible;
+        private FeatureService featSrv = new FeatureService();
+
+        //請求授權時執行
+        public override void OnAuthorization(AuthorizationContext filterContext)
+        {
+            accessible = false;
+            //獲得url請求裡的controller和action
+            string ctrlName = filterContext.RouteData.Values["controller"].ToString();
+            string actName = filterContext.RouteData.Values["action"].ToString();
+            if(IdentityHelper.UserId != null)
+            {
+                int userId = (int)IdentityHelper.UserId;
+                accessible = featSrv.Accessible(userId, ctrlName, actName);
+            }
+            
+
+
+            base.OnAuthorization(filterContext);//進入AuthorizeCore
+        }
+
+        //自定義授權檢查 (return false 則禁止存取頁面)
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+            
+            //return base.AuthorizeCore(httpContext);
+            //return true; // 測試用
+            return accessible;
+            
+        }
+
+        protected override void HandleUnauthorizedRequest(AuthorizationContext filterContext)
+        {
+            /**/
+            //Feature feature = featSrv.GetFeats(IdentityHelper.UserId).FirstOrDefault();
+            var  feat = featSrv.GetFeats(IdentityHelper.UserId).FirstOrDefault();
+
+            if (feat != null)
+            {
+                
+                //導到別頁
+                filterContext.HttpContext.Response.RedirectToRoute(new { controller = feat.ControllerName, action = feat.ActionName });
+            }
+            else
+            {
+                //用URL
+                filterContext.Result = new RedirectToRouteResult(
+                    new System.Web.Routing.RouteValueDictionary {
+                        { "action","Logout"},
+                        { "controller","Home"}
+                });
+            }
+            
+
+            //filterContext.Result = new ViewResult { ViewName = AuthorizationFailView };
+        }
+    }
+}
---
+++ IntelligenceCloud/Models/Partial/ForensicPartial.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace IntelligenceCloud.Models
+{
+    [MetadataType(typeof(ForensicContactMetaData))]
+    public partial class ForensicContact
+    {
+        
+
+        public class ForensicContactMetaData
+        {
+            [Display(Name ="鑑識報告連路人ID")]
+            public int ForensicContactID { get; set; }
+            [Display(Name = "檔案ID")]
+            public Nullable<int> AttachmentId { get; set; }
+            [Display(Name = "分頁號碼")]
+            public Nullable<int> FSheetNum { get; set; }
+            [Display(Name = "分頁名稱")]
+            public string FSheetName { get; set; }
+            [Display(Name = "#")]
+            public Nullable<int> FNum { get; set; }
+            [Display(Name = "名稱")]
+            public string FName { get; set; }
+            [Display(Name = "群組")]
+            public string FGroup { get; set; }
+            [Display(Name = "已修改時間")]
+            [DataType(DataType.DateTime)]
+            [DisplayFormat(DataFormatString ="{0: yyyy/MM/dd HH:mm:ss}",ApplyFormatInEditMode = true,ConvertEmptyStringToNull =true, NullDisplayText ="-")]
+            public Nullable<System.DateTime> FModifiedTime { get; set; }
+            [Display(Name = "條目")]
+            public string FClause { get; set; }
+            [Display(Name = "備註")]
+            public string FNote { get; set; }
+            [Display(Name = "來源")]
+            public string FSource { get; set; }
+            [Display(Name = "已刪除")]
+            public Nullable<bool> isDeleted { get; set; }
+            
+        }
+    }
+
+    [MetadataType(typeof(ForensicSMSMetaData))]
+    public partial class ForensicSMS
+    {
+        
+
+        public class ForensicSMSMetaData
+        {
+            [Display(Name = "鑑識報告簡訊ID")]
+            public int ForensicSMSID { get; set; }
+            [Display(Name = "檔案ID")]
+            public Nullable<int> AttachmentId { get; set; }
+            [Display(Name = "分頁號碼")]
+            public Nullable<int> FSheetNum { get; set; }
+            [Display(Name = "分頁名稱")]
+            public string FSheetName { get; set; }
+            [Display(Name = "#")]
+            public Nullable<int> FNum { get; set; }
+            [Display(Name = "合作對象")]
+            public string FCorrespond { get; set; }
+            [Display(Name = "時間")]
+            [DataType(DataType.DateTime)]
+            [DisplayFormat(DataFormatString = "{0: yyyy/MM/dd HH:mm:ss}", ApplyFormatInEditMode = true, ConvertEmptyStringToNull = true, NullDisplayText = "-")]
+            public Nullable<System.DateTime> FDatetime { get; set; }
+        }
+    }
+}
